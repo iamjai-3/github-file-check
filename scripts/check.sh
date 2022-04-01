712db3ddc64e2ce233213e3239efed0256e3324a
iamjai-dev <jayaprakash@altrosyn.com>
2022-04-01 21:24:15 +0530

    v.1.0

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# if [ -f CheckFile.sh ]; then
+#     echo "CheckFile.sh exists."
+# else
+#     echo "CheckFile.sh does not exist."
+# fi
+
+# [ -f CheckFile.sh ] && echo "This file exists"
+
+# finds if file with given extension already exists!
 
 export DIR=./
-if ls ${DIR}/*.py &>/dev/null; then
+if ls ${DIR}/*.js &>/dev/null; then
     echo "Found."
 else
     echo "Not found."
